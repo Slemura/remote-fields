0f18b14bec8543c04c3afda3cc886f5a4f774fc6
Roman Poshtabayev <titan.growl@gmail.com>
2022-01-31 21:56:00 +0600

    -

--- Runtime/RemoteFields.cs
+++ Runtime/RemoteFields.cs
@@ -54,8 +54,8 @@ namespace com.rpdev.remote_fields.Runtime {
             
             _diff         = false;
             _remote_value = Value;
-            
-            Log($"{key} default value == remote value {EqualityComparer<TValue>.Default.Equals(_remote_value, _default_value)}")
+
+            Log($"{key} default value == remote value {EqualityComparer<TValue>.Default.Equals(_remote_value, _default_value)}");
                 
             _diff         = !EqualityComparer<TValue>.Default.Equals(_remote_value, _default_value);
         }
