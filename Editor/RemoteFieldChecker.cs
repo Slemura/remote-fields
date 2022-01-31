06e459038bb7da9965e3cabe1929d9e3beb6ad37
Roman Poshtabayev <titan.growl@gmail.com>
2022-01-31 21:52:33 +0600

    -

--- Editor/RemoteFieldChecker.cs
+++ Editor/RemoteFieldChecker.cs
@@ -8,10 +8,11 @@ namespace com.rpdev.remote_fields.Editor {
     public static class RemoteFieldChecker {
         
         public static void CheckRemoteFieldsInType(Type type, object instance) {
-            
             object[] remote_fields = type.GetFields()
                                          .Select(field => field.GetValue(instance))
                                          .ToArray();
+            
+            Debug.Log($"[Remote field checker] : total remote fields in {instance} {remote_fields.Length}");
             
             foreach (object remote_field in remote_fields) {
                 MethodInfo method = remote_field.GetType().GetMethod("FetchInfo");
