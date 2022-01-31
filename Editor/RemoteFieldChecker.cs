96bc4db0f6ad8507f24e58c34440358198211939
Roman Poshtabayev <titan.growl@gmail.com>
2022-01-31 21:32:29 +0600

    -Add more logs

--- Editor/RemoteFieldChecker.cs
+++ Editor/RemoteFieldChecker.cs
@@ -2,16 +2,19 @@
 using System.Linq;
 using System.Reflection;
 using com.rpdev.remote_fields.Runtime;
+using UnityEngine;
 
 namespace com.rpdev.remote_fields.Editor {
     public static class RemoteFieldChecker {
         
         public static void CheckRemoteFieldsInType(Type type, object instance) {
             object[] remote_fields = type.GetFields()
-                                         .Where(field => field.FieldType == typeof(RemoteField<>))
+                                         .Where(field => field.FieldType.IsSubclassOf(typeof(RemoteField<>)))
                                          .Select(field => field.GetValue(instance))
                                          .ToArray();
-
+            
+            Debug.Log($"[Remote field checker] : total remote fields in {instance} {remote_fields.Length}");
+            
             foreach (object remote_field in remote_fields) {
                 MethodInfo method = remote_field.GetType().GetMethod("FetchInfo");
                 method?.Invoke(remote_field, Array.Empty<object>());
